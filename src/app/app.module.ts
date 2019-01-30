750a8ea7180fd2fec5315a14ddbe1ebe342f595a
PruessnerN <npruessner@protonmail.com>
2019-01-30 11:22:23 -0600

    Added router and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		HomeComponent
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent
+	}
+];
+
+export const AppRouting = RouterModule.forRoot(routes);
