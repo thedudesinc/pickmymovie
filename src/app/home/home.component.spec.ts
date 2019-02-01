a781dc89583e2cf7fbbb62d923b0856d6d3cbc5b
PruessnerN <npruessner@protonmail.com>
2019-02-01 16:33:04 -0600

    Fixed unit tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './home.component';
+import { CustomMaterialModule } from '../custommaterial.module';
 
 describe('HomeComponent', () => {
 	let component: HomeComponent;
@@ -8,7 +10,13 @@ describe('HomeComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [HomeComponent]
+			declarations: [
+				HomeComponent
+			],
+			imports: [
+				CustomMaterialModule,
+				HttpClientModule
+			]
 		})
 			.compileComponents();
 	}));
