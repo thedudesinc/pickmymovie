8251844a6719b0f004903fab25a5a5231c154c40
PruessnerN <npruessner@protonmail.com>
2019-02-01 17:18:36 -0600

    Fixed unit tests

--- src/app/core/components/add-movie-dialog/add-movie-dialog.component.spec.ts
+++ src/app/core/components/add-movie-dialog/add-movie-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AddMovieDialogComponent } from './add-movie-dialog.component';
+import { CustomMaterialModule } from 'src/app/custommaterial.module';
 
 describe('AddMovieDialogComponent', () => {
 	let component: AddMovieDialogComponent;
@@ -8,7 +12,18 @@ describe('AddMovieDialogComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [AddMovieDialogComponent]
+			declarations: [
+				AddMovieDialogComponent
+			],
+			imports: [
+				CustomMaterialModule,
+				FormsModule,
+				BrowserAnimationsModule
+			],
+			providers: [
+				{ provide: MatDialogRef, useValue: {} },
+				{ provide: MAT_DIALOG_DATA, useValue: [] },
+			]
 		})
 			.compileComponents();
 	}));
